03eff3a51f0ca16e0bcae08cfc0723638fafb933
Aria Estelle Linford <nate.holaday@gmail.com>
2020-04-02 19:21:37 -0700

    pushing to github

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -83,7 +83,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.92;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -174,7 +174,7 @@ static MouseShortcut mshortcuts[] = {
 
 MouseKey mkeys[] = {
 	{ Button4,		ShiftMask,	kscrollup,	{.i = 1} },
-	{ Button5,		ShiftMask,	kscrolldown,	{.i = 1) ),
+	{ Button5,		ShiftMask,	kscrolldown,	{.i = 1} },
 };
 
 /* Internal keyboard shortcuts. */
@@ -187,8 +187,8 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ TERMMOD,              XK_H,  	        zoom,           {.f = +1} },
+	{ TERMMOD,              XK_L,	        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
@@ -197,6 +197,8 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_K,    	kscrollup,      {.i =  1} },
+	{ TERMMOD,              XK_J,    	kscrolldown,    {.i =  1} },
 };
 
 /*
